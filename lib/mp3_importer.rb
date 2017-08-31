bc6b8abeacc048038f1e7db5ac552150c7fba292
Hayden Betts <haydenbetts@gmail.com>
2017-08-31 05:31:43 +0000

    #files method working

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,18 +2,26 @@ class MP3Importer
 
   #MP3Importer.new('./db/mp3s').import
 
-  def self.initialize(path)
+  def initialize(path)
     @path = path
   end
 
   # this should return an array with every file in the path
-  def self.files
-    puts "for testing purposes!"
+  def files
+    # Where the folder variable is the path to the root folder
+    Dir.glob("#{@path}/*.mp3")
   end
 
   # this should make use of methods in the song class to create new instances
   # of songs based on the array
-  def self.import
+
+
+  def import
+  #  @parsed_file_names = []
+  #  self.files.each do |file_name|
+  #    @parse_file_names << file_name.scan(/mp3s\/(.+).mp3/)
+  #  end
+  #  @parsed_file_names
   end
 
 end
